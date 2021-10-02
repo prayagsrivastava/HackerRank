5a2351eba215211ef094b09c7098c0e8786e325d
Prayag <prayag@outlook.in>
2021-10-02 15:02:57 +0530

    strings

---
+++ Practice/Algorithms/caesar-cipher-1.c
@@ -0,0 +1,221 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'caesarCipher' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. INTEGER k
+ */
+
+/*
+ * To return the string from the function, you should either do static allocation or dynamic allocation
+ *
+ * For example,
+ * char* return_string_using_static_allocation() {
+ *     static char s[] = "static allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ * char* return_string_using_dynamic_allocation() {
+ *     char* s = malloc(100 * sizeof(char));
+ *
+ *     s = "dynamic allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ */
+char* caesarCipher(char* s, int k)
+{
+    int character, count = 0;
+    char* l_alphabets = malloc(sizeof(char)*27);
+    for (int c = 97; c<=122; c++)
+    {
+        l_alphabets[count] = (char) c;
+        count++;
+    }
+    l_alphabets[26] = '\0';
+
+    count = 0;
+    char* u_alphabets = malloc(sizeof(char)*27);
+    for (int c = 65; c<=90; c++)
+    {
+        u_alphabets[count] = (char) c;
+        count++;
+    }
+    u_alphabets[26] = '\0';
+
+    printf("%s\n%s\n", u_alphabets, l_alphabets);
+
+    count = 0;
+    char* string = NULL;
+    for (int c = 0, l = strlen(s); c < l; c++)
+    {
+        if (s[c] >= 97 && s[c] <= 122)
+        {
+            for (int d = 0; d < 27; d++)
+                if (s[c] == l_alphabets[d])
+                    character = (int) l_alphabets[(d+k)%26];
+            string = realloc(string, sizeof(char)*(count+2));
+            string[count] = (char) character;
+            string[count+1] = '\0';
+            count++;
+        }
+        else if (s[c] >= 65 && s[c] <= 90)
+        {
+            for (int d = 0; d < 27; d++)
+                if (s[c] == u_alphabets[d])
+                    character = (int) u_alphabets[(d+k)%26];
+            string = realloc(string, sizeof(char)*(count+2));
+            string[count] = (char) character;
+            string[count+1] = '\0';
+            count++;
+        }
+        else
+        {
+            string = realloc(string, sizeof(char)*(count+2));
+            string[count] = s[c];
+            string[count+1] = '\0';
+            count++;
+        }
+    }
+    free(u_alphabets);
+    free(l_alphabets);
+    return string;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    int n = parse_int(ltrim(rtrim(readline())));
+
+    char* s = readline();
+
+    int k = parse_int(ltrim(rtrim(readline())));
+
+    char* result = caesarCipher(s, k);
+
+    fprintf(fptr, "%s\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
---
+++ Practice/Algorithms/strong-password.c
@@ -0,0 +1,170 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'minimumNumber' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. STRING password
+ */
+int in_string(char* s, char c)
+{
+    for (int x = 0, l = strlen(s); x < l; x++)
+        if (c == s[x])
+            return 1;
+    return 0;
+}
+
+int minimumNumber(int n, char* password)
+{
+    int digits = 1, ucletters = 1, lcletters = 1, schars = 1;
+    for (int c = 0; c < n; c++)
+    {
+        if (digits && (password[c] >= 48 && password[c] <= 57))
+            digits = 0;
+        if (ucletters && (password[c] >= 65 && password[c] <= 90))
+            ucletters = 0;
+        if (lcletters && password[c] >= 97 && password[c] <= 122)
+            lcletters = 0;
+        if (schars && in_string("!@#$%^&*()-+", password[c]))
+            schars = 0;
+    }
+
+    int count = (digits + ucletters + lcletters + schars);
+    if (n < 6 && (6 - n) >= count)
+        return (6 - n);
+    else
+        return count;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    int n = parse_int(ltrim(rtrim(readline())));
+
+    char* password = readline();
+
+    int answer = minimumNumber(n, password);
+
+    fprintf(fptr, "%d\n", answer);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
